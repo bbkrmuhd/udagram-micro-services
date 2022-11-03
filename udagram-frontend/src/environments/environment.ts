5c7f1c98543bb3fbde6d934db0273e32efea6fc1
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-11-03 07:18:04 +0100

    refactor: update api url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a90d1b9e1c35342a98b4675e1176c847-179370177.us-east-2.elb.amazonaws.com'
 };
 
 /*
